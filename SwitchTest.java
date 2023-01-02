e2a861e46a1d34598d3047e2acbbed18b74807ba
Nafizanowrin <115865205+Nafizanowrin@users.noreply.github.com>
2023-01-02 19:25:05 +0600

    Add files via upload

---
+++ SwitchTest.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class SwitchTest {
+
+    public static void main(String[] args) {
+        Switch Switch= new Switch();
+
+        Switch.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Switch.setSize(900, 500);
+        Switch.setVisible(true);
+    }
+} 
